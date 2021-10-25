197e9386e14ad387e3a0a607b086612f4b51e025
sommai <sstone@pulte.come>
2021-10-25 11:03:46 -0700

    Test PR

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -51,7 +51,7 @@ namespace FunctionApp1
             }
             catch(NullReferenceException)
             {
-                Console.WriteLine("Test pull request");
+                Console.WriteLine("Test pull request 2");
             }
         }
     }
