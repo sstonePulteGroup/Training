a63589aea537ec8a368f72c7f1a2b53ac5244866
sommai <sstone@pulte.come>
2021-10-26 15:29:54 -0700

    test

--- FunctionApp1/Function1.cs
+++ FunctionApp1/Function1.cs
@@ -51,7 +51,7 @@ namespace FunctionApp1
             }
             catch(NullReferenceException)
             {
-                Console.WriteLine("Test1");
+                Console.WriteLine("Test2");
             }
         }
     }
